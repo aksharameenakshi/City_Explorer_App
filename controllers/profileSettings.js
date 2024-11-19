e0e414d107fce830ed3c7061d45496aad4ca6a4f
Aboyo23 <ridwanoyenuga@gmail.com>
2024-11-19 09:08:47 -0500

    Update profileSettings.js

--- controllers/profileSettings.js
+++ controllers/profileSettings.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { ProfileSettingsModel } from "../models/Behavoir.js";
-import { AboutUsModel , TermsAndConditionModel ,PrivacyPolicyModel} from "../models/Behavoir.js";
 const route = express.Router();
 route.use(express.json());
 
@@ -39,48 +38,3 @@ export const getProfileSettings =async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   }
-
-
-    export const getAboutUs = async (req, res) => {
-        try {
-            const aboutUs = await AboutUsModel.find();
-            if (aboutUs.length > 0) {
-                res.json(aboutUs);
-            } else {
-                res.status(404).json({ error: 'Description not found' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    };
-    export const getTermsAndCondition = async (req, res) => {
-        try {
-            const termsAndCondition = await TermsAndConditionModel.find();
-            if (termsAndCondition.length > 0) {
-    
-                res.json(termsAndCondition)
-            } else {
-                res.status(404).json({ error: 'Description not found' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    };
-    export const getPrivacyPolicy = async (req, res) => {
-        try {
-            const privacyPolicy = await PrivacyPolicyModel.find();
-    
-            if (privacyPolicy.length > 0) {
-    
-                res.json(privacyPolicy);
-            } else {
-                res.status(404).json({ error: 'Description not found' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    };
-
